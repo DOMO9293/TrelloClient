ff8d69b7fc4ecc2fde05dfd63a3d7eb4ad5b4b80
DOMO9293 <flqjslgmail.com>
2020-01-03 05:20:44 +0100

    started

---
+++ trelloclone/src/pages/main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+const Mainpage = () => {
+  return (
+    <div>
+      <h1>Hallo‚</h1>
+    </div>
+  );
+};
+
+export default Mainpage;
